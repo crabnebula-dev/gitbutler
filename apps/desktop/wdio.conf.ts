7080cfdc4467c85102ead875f3db79dcf684af34
Julian Harty <julian@captur.ai>
2025-07-31 11:34:50 +0100

    Adding macOS webdriver support.
    Still various details to resolve. Pushing for ease of discussion.

--- apps/desktop/wdio.conf.ts
+++ apps/desktop/wdio.conf.ts
@@ -1,11 +1,22 @@
-import { TestRecorder } from './e2e/record.js';
-import { spawn, type ChildProcess } from 'node:child_process';
+import { TestRecorder } from './e2e/record.ts';
+import { spawn, spawnSync, type ChildProcess } from 'node:child_process';
 import os from 'node:os';
 import path from 'node:path';
 import type { Options, Frameworks } from '@wdio/types';
 
+import { waitTauriDriverReady } from "@crabnebula/tauri-driver";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const isMac = process.platform === 'darwin';
+
+console.info(`Running tests on ${isMac ? 'macOS' : 'non-macOS'} platform.`);
+console.info(`Process platform: ${process.platform}`);
+
 const videoRecorder = new TestRecorder();
 let tauriDriver: ChildProcess;
+let exit = false;
+let testRunnerBackend: ChildProcess;
 
 export const config: Options.WebdriverIO = {
 	hostname: '127.0.0.1',
@@ -17,7 +28,7 @@ export const config: Options.WebdriverIO = {
 			// @ts-expect-error custom tauri capabilities
 			maxInstances: 1,
 			'tauri:options': {
-				application: '../../target/debug/gitbutler-tauri'
+				application: path.resolve(__dirname, '../../target/debug/gitbutler-tauri'),
 			}
 		}
 	],
@@ -39,6 +50,42 @@ export const config: Options.WebdriverIO = {
 	connectionRetryTimeout: 120000,
 	connectionRetryCount: 0,
 
+	onPrepare: function () {
+		spawnSync(
+			"pnpm",
+			["tauri", "build", "--debug", "--features", "automation"].concat(
+				process.platform === 'darwin' ? ["-b", "app"] : ["--no-bundle"]
+			),
+			{
+				cwd: path.resolve(__dirname, '..'),
+				stdio: 'inherit',
+				shell: true
+			});
+
+
+			if (isMac) {
+			if (!process.env.CN_API_KEY) {
+				console.error('CN_API_KEY env var is not set, which is required for test-runner-backend.');
+				process.exit(1);
+			}
+			testRunnerBackend = spawn('npm', ["run", "test-runner-backend"], {
+				stdio: 'inherit',
+				shell: true,
+			});
+			testRunnerBackend.on('error', (error) => {
+				console.error('Test runner backend error:', error);
+				process.exit(1);
+			});
+			testRunnerBackend.on('exit', (code) => {
+				if (!exit) {
+				console.error('Test runner backend exited with code:', code);
+				process.exit(1);
+				}
+			});
+			process.env.REMOTE_WEBDRIVER_URL = 'http://127.0.0.1:3000';
+    }
+  },
+
 	beforeTest: async function (test: Frameworks.Test) {
 		const videoPath = path.join(import.meta.dirname, '/e2e/videos');
 		videoRecorder.start(test, videoPath);
@@ -50,10 +97,22 @@ export const config: Options.WebdriverIO = {
 	},
 
 	// ensure we are running `tauri-driver` before the session starts so that we can proxy the webdriver requests
-	beforeSession: () =>
-		(tauriDriver = spawn(path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'), [], {
-			stdio: [null, process.stdout, process.stderr]
-		})),
+	beforeSession: () => {
+		if (isMac) {
+			// On macOS, we use the tauri-driver package directly.
+			//
+			console.info('Remote Webdriver URL is: ', process.env.REMOTE_WEBDRIVER_URL);
+			tauriDriver = spawn('pnpm', ['tauri-driver'], {
+				stdio: [null, process.stdout, process.stderr],
+			});
+			waitTauriDriverReady(tauriDriver);
+			console.info('Tauri driver ready.');
+		} else {
+			(tauriDriver = spawn(path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'), [], {
+				stdio: [null, process.stdout, process.stderr]
+			}));
+		}
+	},
 
 	afterSession: () => {
 		tauriDriver.kill();
@@ -63,3 +122,4 @@ export const config: Options.WebdriverIO = {
 async function sleep(ms: number) {
 	return await new Promise((resolve) => setTimeout(resolve, ms));
 }
+
